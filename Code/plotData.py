7cd5e1c6da485cb894d166ea0313681c77b61564
Matias Saavedra Silva <38446717+matias9927@users.noreply.github.com>
2021-05-08 15:41:06 -0400

    Create plotData.py

---
+++ Code/plotData.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+data = []
+subplotNum = [421, 422, 423, 424, 425, 426, 427, 428]
+colors = ['r', 'g', 'b', 'y', 'm', 'c', 'k', 'w']
+for i in range(8):
+	plt.subplot(subplotNum[i])
+	file = open("data_chan" + str(i) + "_0.txt", 'r')
+	for line in file.readlines():
+		data.append(int(line))
+	plt.plot(data, colors[i])
+	data = []
+plt.show()
