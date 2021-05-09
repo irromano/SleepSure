ff5790565c40ac4a528f1a84269d0c0c553944c6
Matias S <matias@udel.edu>
2021-05-09 15:55:53 -0400

    Sends 5-byte packets

--- Code/BluetoothSend/BluetoothSend.ino
+++ Code/BluetoothSend/BluetoothSend.ino
@@ -2,20 +2,41 @@
 //Author: Matias Saavedra Silva
 
 #include "BluetoothSerial.h"
+#include <ADS1299.h>
+
+#define BME_CS 14
+#define ADS_DRDY 15
+#define ADS_START 27
+#define ADS_RESET 32
+
+#define CHANNELS 8
+
+SleepSure_ADS1299 ads(BME_CS, ADS_DRDY, ADS_START, ADS_RESET);
+
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
-int counts[8];
-bool synced = false;
+//int counts[8];
+int *counts;
+bool synced = true;
 int recv;
 
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("SleepSure"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
+
+  bool pass = ads.begin();
+    if (!pass)
+    {
+        Serial.println("ADS1299 ID not recieved correctly.");
+    }
+    Serial.print("Detected sensor ID: ");
+    Serial.println(ads.getID(), 16);
+    counts = new int[CHANNELS];
 }
 
 void getData(int nums[]) {
@@ -27,24 +48,41 @@ void getData(int nums[]) {
 void loop() {
   // Do not start sending data unless the receiver is ready
   if(synced) {
-    getData(counts);
+    ads.readChannels(counts, CHANNELS);
     // Iterate through each channel and send the data one byte at a time
     // If the data takes only one byte, pad it with 0x00. The data from each channel with its padding will always be 3 bytes
     for(int i=0; i<8;i++) {
       if(counts[i] < 256) {
         SerialBT.write(counts[i]);
+        SerialBT.write(0);
+        SerialBT.write(0);
         SerialBT.write(0); 
       }
-      else {
+      if(counts[i] < 65536) {
         SerialBT.write(counts[i]>>8);
+        SerialBT.write(counts[i] & 0xff);
+        SerialBT.write(0);
+        SerialBT.write(0);
+      }
+      if(counts[i] < 16777216) {
+        SerialBT.write(counts[i]>>16);
+        SerialBT.write((counts[i]>>8) & 0xff);
+        SerialBT.write(counts[i] & 0xff);
+        Serial.println("Heel " + (counts[i]>>16));
+        SerialBT.write(0);
+      }
+      else {
+        SerialBT.write(counts[i]>>24);
+        SerialBT.write((counts[i]>>16) & 0xff);
+        SerialBT.write((counts[i]>>8) & 0xff);
         SerialBT.write(counts[i] & 0xff);
       }
       SerialBT.write(0);
     }
     // After all the data is sent, mark the end of the data with 0x0010
-    SerialBT.write(0);
-    SerialBT.write(10);
-    delay(3);
+    SerialBT.write(255);
+    SerialBT.write(255);
+    delay(10);
   }
   else {
     // Wait for the receiver to send 0xff
