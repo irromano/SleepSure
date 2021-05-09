4e07392cc19d50e41b449f582797482c326f58a7
Matias S <matias@udel.edu>
2021-05-09 18:58:54 -0400

    Plot data fixed

--- Code/plotData.py
+++ Code/plotData.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 data = []
 subplotNum = [421, 422, 423, 424, 425, 426, 427, 428]
-colors = ['r', 'g', 'b', 'y', 'm', 'c', 'k', 'w']
+colors = ['r', 'g', 'b', 'y', 'm', 'c', 'k', 'r']
 for i in range(8):
 	plt.subplot(subplotNum[i])
-	file = open("data_chan" + str(i) + "_0.txt", 'r')
+	file = open("Data\\data_chan" + str(i) + "_0.txt", 'r')
 	for line in file.readlines():
 		data.append(int(line))
 	plt.plot(data, colors[i])
